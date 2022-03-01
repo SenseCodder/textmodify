05755247c656eba8cbc2fcf9398e548710b616a6
Rashmita Dudhat <rashmitaduhdat26@gmail.com>
2022-03-01 13:31:09 +0530

    first commit

---
+++ scrapemaster/views.py
@@ -0,0 +1,69 @@
+#i have create this file - rashmi
+
+
+from debugpy import debug_this_thread
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'index.html')
+
+def analyze(request):
+    djtext = request.POST.get('text', 'default')
+    removepunc=request.POST.get('removepunc','off')
+    fullcaps=request.POST.get('fullcaps','off')
+    newlineremover=request.POST.get('newlineremover','off')
+    extraspaceremover=request.POST.get('extraspaceremover','off')
+    
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+       
+    
+    if fullcaps=="on":
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+    
+    
+    if newlineremover=="on":
+        analyzed=""
+        for char in djtext:
+            if char!="\n":
+                analyzed=analyzed+char
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext = analyzed
+    if(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+     
+    if(removepunc != "on" and fullcaps != "on" and newlineremover != "on" and extraspaceremover != "on"):
+        return HttpResponse("Error")  
+    
+    
+    
+    return render(request, 'analyze.html', params)
+
+def ex(request):
+    sites = ['''<h1>For Entertainment </h1><a href = "https://www.youtube.com" >youtube video</a>''',
+             '''<h1>For Interaction </h1><a href = "https://www.facebook.com" >Facebook</a>''',
+             '''<h1>For Insight   </h1><a href = "https://www.ted.com/talks" >Ted Talk</a>''',
+             '''<h1>For Internship   </h1><a href="https://internshala.com" >Intenship</a>''',
+             ]
+    return HttpResponse((sites))
+
+
+
+
+
